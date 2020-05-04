f689a0b35ec00f4345b053e59e2c4fc83dbf6098
Priyam <37201646+priyam1098@users.noreply.github.com>
2020-05-04 18:16:32 +0530

    Create 1D Arrays in C.c

---
+++ 1D Arrays in C.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n,i;
+    long int sum=0;
+    scanf("%d",&n);
+    int *arr = (int*)malloc(n * sizeof(int));
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        sum=sum+arr[i];
+    }
+    printf("%ld",sum);
+    free(arr); 
+    return 0;
+}
